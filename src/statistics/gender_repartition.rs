826404f9cd48ad3296f4d1d435631705c8b61b09
Maxence Cornaton <22578053+maxence-cornaton@users.noreply.github.com>
2025-06-25 19:24:44 +0200

    Make `draw_bar` generic function

--- src/statistics/gender_repartition.rs
+++ src/statistics/gender_repartition.rs
@@ -49,11 +49,13 @@ fn draw_graph_by_gender_by_event(convention: &Convention, year: u16) {
             (0..events_count)
                 .zip(data.iter())
                 .map(|(x, (event, counts))| {
-                    let x = x as f32;
+                    let x = x as f32 * 2.0;
 
-                    let (female_bar, count_female) = draw_females(x, *counts.get(&Female).unwrap_or(&0));
-                    let (male_bar, count_male) = draw_males(x, *counts.get(&Male).unwrap_or(&0));
-                    let label = draw_label(x, event.name().as_str());
+                    let (female_bar, count_female) =
+                        draw_bar(x, *counts.get(&Female).unwrap_or(&0), true, false, MAGENTA);
+                    let (male_bar, count_male) =
+                        draw_bar(x + 1.0, *counts.get(&Male).unwrap_or(&0), false, true, BLUE);
+                    let label = draw_label(x, event.name().as_str(), 2);
 
                     vec![
                         female_bar.into_dyn(),
@@ -95,50 +97,34 @@ fn group_by_gender_by_event(convention: &Convention) -> BTreeMap<&Event, HashMap
         .collect()
 }
 
-fn draw_females<'a>(x: f32, count: u64) -> (Rectangle<(f32, i32)>, Text<'a, (f32, i32), String>)
-{
-    let x0 = x * 2.0;
-    let x1 = x * 2.0 + 1.0;
-    let females_count = count as i32;
-    let mut female_bar = Rectangle::new([(x0, 0), (x1, females_count)], MAGENTA.filled());
-    female_bar.set_margin(0, 0, 5, 1);
+fn draw_bar<'a>(
+    x: f32,
+    count: u64,
+    first_of_group: bool,
+    last_of_group: bool,
+    color: RGBColor
+) -> (Rectangle<(f32, i32)>, Text<'a, (f32, i32), String>) {
+    let x0 = x;
+    let x1 = x + 1.0;
+    let count = count as i32;
+    let mut bar = Rectangle::new([(x0, 0), (x1, count)], color.filled());
+    let left_margin = if first_of_group { 5 } else { 1 };
+    let right_margin = if last_of_group { 5 } else { 1 };
+    bar.set_margin(0, 0, left_margin, right_margin);
 
     let font_desc =
         FontDesc::new(FontFamily::SansSerif, 16_f64, FontStyle::Normal).transform(Rotate270);
-    let count_female = Text::new(
-        females_count.to_string(),
-        (x * 2.0 + 0.25, females_count + 1),
-        font_desc.clone(),
-    );
+    let count_label = Text::new(count.to_string(), (x + 0.25, count + 1), font_desc.clone());
 
-    (female_bar, count_female)
+    (bar, count_label)
 }
 
-fn draw_males<'a>(x: f32, count: u64) -> (Rectangle<(f32, i32)>, Text<'a, (f32, i32), String>)
-{
-    let x0 = x * 2.0 + 1.0;
-    let x1 = x * 2.0 + 2.0;
-    let males_count = count as i32;
-    let mut male_bar = Rectangle::new([(x0, 0), (x1, males_count)], BLUE.filled());
-    male_bar.set_margin(0, 0, 1, 5);
-
+fn draw_label<'a>(x: f32, event_name: &str, number_of_bars: usize) -> Text<'a, (f32, i32), String> {
     let font_desc =
-        FontDesc::new(FontFamily::SansSerif, 16_f64, FontStyle::Normal).transform(Rotate270);
-    let count_male = Text::new(
-        males_count.to_string(),
-        (x * 2.0 + 1.25, males_count + 1),
-        font_desc.clone(),
-    );
-
-    (male_bar, count_male)
-}
-
-fn draw_label<'a>(x: f32, event_name: &str) -> Text<'a, (f32, i32), String> {
-    let font_desc = FontDesc::new(FontFamily::SansSerif, 16_f64, FontStyle::Normal)
-        .transform(Rotate90);
+        FontDesc::new(FontFamily::SansSerif, 16_f64, FontStyle::Normal).transform(Rotate90);
     Text::new(
         format!("  {}", event_name),
-        (x * 2.0 + 1.25, -1),
+        (x + 0.25 + (number_of_bars as f32 / 2.0), -1),
         font_desc.clone(),
     )
 }
