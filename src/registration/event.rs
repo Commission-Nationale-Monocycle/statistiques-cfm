92efdc48ed9ba31af55c712eceab86d8adc0dc9a
Maxence Cornaton <22578053+maxence-cornaton@users.noreply.github.com>
2025-06-25 15:41:41 +0200

    Sort events by name then by index

--- src/registration/event.rs
+++ src/registration/event.rs
@@ -1,10 +1,18 @@
+use std::cmp::Ordering;
 use derive_getters::Getters;
 
-#[derive(Debug, Getters, PartialOrd, PartialEq, Ord, Eq, Hash)]
+#[derive(Debug, Getters, PartialOrd, PartialEq, Eq, Hash)]
 pub struct Event {
     /// The index refers to the column's index, starting from the first event
     index: usize,
     name: String
+}
+
+impl Ord for Event {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.name.cmp(&other.name)
+            .then(self.index.cmp(&other.index))
+    }
 }
 
 impl Event {
